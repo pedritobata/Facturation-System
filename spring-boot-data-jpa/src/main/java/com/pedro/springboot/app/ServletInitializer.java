d032f8736141fb89c9106da03a741ad3dc48cf06
unknown <pedromartinez_goyo@hotmail.com>
2019-06-25 16:11:05 -0500

    Changes needed to deploy in external server

---
+++ spring-boot-data-jpa/src/main/java/com/pedro/springboot/app/ServletInitializer.java
@@ -0,0 +1,17 @@
+package com.pedro.springboot.app;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+public class ServletInitializer extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(SpringBootDataJpaApplication.class);
+	}
+
+	
+	
+}
